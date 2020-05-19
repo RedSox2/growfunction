74cf8312e3a9318bf6fc0655024567fc8fee4f62
RedSox2 <65141925+RedSox2@users.noreply.github.com>
2020-05-19 10:39:37 -0700

    Add files via upload

---
+++ growfunc.js
@@ -0,0 +1,7 @@
+echo "# growfunction" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/RedSox2/growfunction.git
+git push -u origin master
+                
