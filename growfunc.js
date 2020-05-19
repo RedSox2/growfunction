6f2d42029adfc6e5b29b1b2ca072ad70c2998a00
RedSox2 <65141925+RedSox2@users.noreply.github.com>
2020-05-19 10:43:21 -0700

    Update growfunc.js

--- growfunc.js
+++ growfunc.js
@@ -1,7 +1,18 @@
-echo "# growfunction" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/RedSox2/growfunction.git
-git push -u origin master
-                
+var grow = function() {
+	// Step 1. What element do we want to animate?
+   var ohnoes = document.getElementById("ohnoes");
+   ohnoes.style.width = "50px";
+  
+   // Step 2. What function will change it each time?
+   var startTime = new Date().getTime();
+   var makeItBigger = function() {
+       var currTime = new Date().getTime();
+       var newWidth = (50 + ((currTime - startTime)/1000) * 30);
+       ohnoes.style.width = newWidth + "px"; 
+       
+        if (newWidth < 300) {
+            window.requestAnimationFrame(makeItBigger);
+		}
+   };
+	
+};
